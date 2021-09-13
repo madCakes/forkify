4b1cf9414e5031bea028342a8dd91f9f9f4ff53f
madCakes <jason.pbrooks@hotmail.com>
2021-09-13 12:54:08 +0100

    Guard clause in controlRecipes to fix typeError undefined on bookmarks. This causes an issue with the highlighting in the bookmarks view.

--- src/js/model.js
+++ src/js/model.js
@@ -10,6 +10,7 @@ export const state = {
     resultsPerPage: RES_PER_PAGE,
     page: 1,
   },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
@@ -28,6 +29,11 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
+    if (state.bookmarks.some(bookmark => bookmark.id === id))
+      state.recipe.bookmarked = true;
+    else state.recipe.bookmarked = false;
+
     console.log(recipe);
   } catch (err) {
     // temp error handling
@@ -35,6 +41,7 @@ export const loadRecipe = async function (id) {
     throw err;
   }
 };
+
 export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
@@ -56,7 +63,7 @@ export const loadSearchResults = async function (query) {
   }
 };
 
-export const getSearchResultsPage = function (page = state.search.page) {
+export const getSearchResultsPage = function (page = 1) {
   state.search.page = page;
 
   const start = (page - 1) * state.search.resultsPerPage; // 0
@@ -74,3 +81,35 @@ export const updateServings = function (newServings) {
 
   state.recipe.servings = newServings;
 };
+
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
+export const addBookmark = function (recipe) {
+  // Add Bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+
+  persistBookmarks();
+};
+
+export const deleteBookmark = function (id) {
+  // Delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmark
+  if (id === state.recipe.id) state.recipe.bookmarked = false;
+
+  persistBookmarks();
+};
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
+console.log(state.bookmarks);
